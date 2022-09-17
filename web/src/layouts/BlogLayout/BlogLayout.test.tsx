aab6327515c545f7232e7fa56e367a0e0d979b26
Bruno Mota <bruno.ed.mota@gmail.com>
2022-09-16 22:25:08 -0400

    Added layout and fixed up home and about page

---
+++ web/src/layouts/BlogLayout/BlogLayout.test.tsx
@@ -0,0 +1,15 @@
+import { render } from '@redwoodjs/testing/web'
+
+import BlogLayout from './BlogLayout'
+import BlogLayoutStories from './BlogLayout.stories'
+
+//   Improve this test with help from the Redwood Testing Doc:
+//   https://redwoodjs.com/docs/testing#testing-pages-layouts
+
+describe('BlogLayout', () => {
+  it('renders successfully', () => {
+    expect(() => {
+      render(<BlogLayout />)
+    }).not.toThrow()
+  })
+})
