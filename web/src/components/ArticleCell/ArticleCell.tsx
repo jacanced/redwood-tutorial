a44a5fff35fe69577ecb2f48678af94571e56f4f
Bruno Mota <bruno.ed.mota@gmail.com>
2022-09-17 17:52:58 -0400

    Made article page

--- web/src/components/ArticleCell/ArticleCell.tsx
+++ web/src/components/ArticleCell/ArticleCell.tsx
@@ -1,11 +1,11 @@
 import Article from 'src/components/Article'
 
-import type { ArticlesQuery } from 'types/graphql'
+import type { ArticleQuery } from 'types/graphql'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
 export const QUERY = gql`
-  query ArticlesQuery {
-    articles: posts {
+  query ArticleQuery($id: Int!) {
+    article: post(id: $id) {
       id
       title
       body
@@ -22,12 +22,6 @@ export const Failure = ({ error }: CellFailureProps) => (
   <div style={{ color: 'red' }}>Error: {error.message}</div>
 )
 
-export const Success = ({ articles }: CellSuccessProps<ArticlesQuery>) => {
-  return (
-    <>
-      {articles.map((article) => (
-        <Article key={article.id} article={article} />
-      ))}
-    </>
-  )
+export const Success = ({ article }: CellSuccessProps<ArticleQuery>) => {
+  return <Article article={article} />
 }
